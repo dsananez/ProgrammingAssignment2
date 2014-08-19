91fe72a04af2fb31d4ebc361a75d95c5b127fa00
dsananez <dsananez@gmail.com>
2014-08-19 18:02:34 -0430

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions calculates the inverse of a submited matrix. It then 
+## caches that value to use it again over and over without making the
+## calculation each time.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        imat <- NULL
+        set <- function(y) {
+                x <<- y
+                imat <<- NULL
+        }
+        get <- function() x
+        setimat <- function(invmat) imat <<- invmat
+        getimat <- function() imat
+        list(set = set, get = get,
+             setimat = setimat,
+             getimat = getimat)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+                imat <- x$getimat()
+                if(!is.null(imat)) {
+                        message("getting cached data")
+                        return(imat)
+                }
+                data <- x$get()
+                imat <- solve(data)
+                x$setimat(imat)
+                imat
 }
